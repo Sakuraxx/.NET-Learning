e3639ffb919a161f0ecabef1f511d5b6433b2181
Xia, Jing Wen <amyfor7@gmail.com>
2025-01-30 17:36:01 +0800

    feat: finish the Simula’s Soup challenge

---
+++ TheC#PlayerGuide/17_SimulasSoup/Entrance.cs
@@ -0,0 +1,55 @@
+﻿using Spectre.Console;
+
+namespace OOP.Tuples;
+
+public static class SimulasSoup
+{
+    public enum Type
+    {
+        Soup,
+        Stew,
+        Gumbo
+    };
+
+    public enum Ingredient
+    {
+        Mushrooms,
+        Chicken,
+        Carrots,
+        Potatoes
+    };
+
+    public enum Seasoning
+    {
+        Spicy,
+        Salty,
+        Sweet
+    };
+
+    public static (Type, Ingredient, Seasoning) AskUserFoodChoices()
+    {
+        Type type = AnsiConsole.Prompt(
+                    new SelectionPrompt<Type>()
+                    .Title("What type of recipe do you want to cook?")
+                    .AddChoices([Type.Soup, Type.Stew, Type.Gumbo]));
+
+        Ingredient ingredient = AnsiConsole.Prompt(
+            new SelectionPrompt<Ingredient>()
+            .Title("What type of ingredient do you want to cook?")
+            .AddChoices([Ingredient.Mushrooms, Ingredient.Chicken, Ingredient.Carrots, Ingredient.Potatoes]));
+
+        Seasoning seasoning = AnsiConsole.Prompt(
+            new SelectionPrompt<Seasoning>()
+            .Title("What type of recipe do you want to cook?")
+            .AddChoices([Seasoning.Spicy, Seasoning.Salty, Seasoning.Sweet]));
+
+        return (type, ingredient, seasoning);
+    }
+
+
+    public static void Main()
+    {
+        (Type type, Ingredient ingredient, Seasoning seasoning) = AskUserFoodChoices();
+        AnsiConsole.MarkupLine($"Congrats, you made the [cyan]{seasoning} {ingredient} {type}[/]!");
+    }
+}
