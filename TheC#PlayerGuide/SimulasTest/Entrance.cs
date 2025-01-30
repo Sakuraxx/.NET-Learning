fa23da5f076c8d3633ed69b71d86361f89ff1594
Xia, Jing Wen <amyfor7@gmail.com>
2025-01-30 15:49:19 +0800

    feat: BoxState transition cycle

---
+++ TheC#PlayerGuide/SimulasTest/Entrance.cs
@@ -0,0 +1,51 @@
+﻿using Spectre.Console;
+
+namespace SimulasTest;
+
+public static class Entrance 
+{
+    enum BoxState
+    {
+        Open,
+        Closed,
+        Locked
+    };
+
+    public static void Main()
+    {
+        BoxState boxState = BoxState.Locked;
+
+        while(true)
+        {
+            var choice = AnsiConsole.Prompt(
+                    new TextPrompt<string>($"The chest is [green]{boxState.ToString()}[/]. What do you want to do?")
+                    .PromptStyle("cyan"));
+            choice = choice.ToLower();
+            switch(boxState)
+            {
+                case BoxState.Open:
+                    if(choice == "close")
+                    {
+                        boxState = BoxState.Closed;
+                    }
+                    break;
+                case BoxState.Closed:
+                    if(choice == "lock")
+                    {
+                        boxState = BoxState.Locked;
+                    }
+                    else if(choice == "open")
+                    {
+                        boxState = BoxState.Open;
+                    }
+                    break;
+                case BoxState.Locked:
+                    if(choice == "unlock")
+                    {
+                        boxState = BoxState.Closed;
+                    }
+                    break;
+            }
+        }
+    }
+}
