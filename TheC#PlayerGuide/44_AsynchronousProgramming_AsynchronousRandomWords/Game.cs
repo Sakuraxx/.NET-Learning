cca6751fc0c42a21540621a9b9721f33a202b338
Xia, Jing Wen <amyfor7@gmail.com>
2025-02-07 20:29:16 +0800

    feat: random recreation

---
+++ TheC#PlayerGuide/44_AsynchronousProgramming_AsynchronousRandomWords/Game.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using Spectre.Console;
+
+namespace AdvancedTopic.AsynchronousProgramming;
+
+public class Game
+{
+    public int RandomlyRecreate(string word)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+        bool isMatch = false;
+        int attemptTiems = 0;
+        do
+        {
+            stringBuilder.Clear();
+            int len = word.Length;
+            for (int i = 0; i < len; i++)
+            {
+                stringBuilder.Append((char)('a' + random.Next(26)));
+            }
+            if (stringBuilder.ToString().Equals(word))
+            {
+                isMatch = true;
+            }
+            attemptTiems++;
+        } while (!isMatch);
+        return attemptTiems;
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Game game = new Game();
+        string word = AnsiConsole.Prompt(
+            new TextPrompt<string>("Please input a word, no more than 5 characters.")
+            .PromptStyle("cyan"));
+        int times = game.RandomlyRecreate(word);
+        AnsiConsole.MarkupLineInterpolated($"Take [green]{times}[/] random creations to recreate this word.");
+    }
+
+}
