040c70127372e219e10a341a2833cf4f11aec688
Xia, Jing Wen <amyfor7@gmail.com>
2025-02-07 17:28:25 +0800

    feat: generate random numbers using a thread

---
+++ TheC#PlayerGuide/43_Threads_TheRepeatingStream/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Spectre.Console;
+
+namespace AdvancedTopic.Threads;
+
+public class GameController
+{
+    public static void GenerateRandomNumbers(object? recentNumbers)
+    {
+        if(recentNumbers == null || recentNumbers is not RecentNumbers) { return; }
+        RecentNumbers recNumbers = (RecentNumbers)recentNumbers;
+        Random random = new Random();
+        List<int> result = new List<int>();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (true)
+        {
+            result.Clear();
+            stringBuilder.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                result.Add(random.Next(10));
+                stringBuilder.Append(result[i]);
+            }
+            recNumbers.Numbers = result;
+            AnsiConsole.MarkupLineInterpolated($"Generate: [blue]{stringBuilder.ToString()}[/]");
+            Thread.Sleep(1000);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        RecentNumbers recentNumbers = new RecentNumbers();
+        Thread randomNumGenThread = new Thread(GenerateRandomNumbers);
+        randomNumGenThread.Start(recentNumbers);
+    }
+
+}
---
+++ TheC#PlayerGuide/43_Threads_TheRepeatingStream/RecentNumbers.cs
@@ -0,0 +1,6 @@
+﻿namespace AdvancedTopic.Threads;
+
+public class RecentNumbers
+{
+    public List<int> Numbers {  get; set; }
+}
