b4abf9e608430fa5cccbfb2ed6636e447de330a5
cactus <amyfor7@gmail.com>
2025-03-22 19:21:46 -0700

    feat: only procedural code

---
+++ TheC#PlayerGuide/40_QueryExpression_TheThreeLenses/40_QueryExpression_TheThreeLenses/GameController.cs
@@ -0,0 +1,39 @@
+﻿namespace AdvancedTopic.QueryExpression;
+public class GameController
+{
+    public IEnumerable<int> NormalThreelens(int[] nums)
+    {
+        List<int> res = new List<int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] % 2 == 0)
+            {
+                res.Add(i);
+            }
+        }
+
+        res.Sort();
+
+        for (int i = 0; i < res.Count; i++) 
+        {
+            res[i] *= 2;
+        }
+
+        return res;
+    }
+
+
+    public void Run()
+    {
+        int[] nums = { 1, 9, 2, 8, 3, 7, 4, 6, 5 };
+        IEnumerable<int> res = this.NormalThreelens(nums);
+        res.ToList().ForEach(n => Console.Write(n + " "));
+    }
+
+
+    public static void Main(string[] args)
+    {
+        GameController controller = new GameController();
+        controller.Run();
+    }
+}
